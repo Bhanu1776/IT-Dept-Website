492045f19a286de61d55917c9553e1b6fc2e44f9
SohamDesai1 <sohamdesai123@gmail.com>
2023-03-05 18:35:12 +0530

    chore(added firebase setup): added the firebase configuration in the project

---
+++ lib/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGIN_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+const analytics = getAnalytics(app);
