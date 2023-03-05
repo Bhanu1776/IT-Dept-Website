ae3304c22f7936a717b071e0f77bca56c2c5db0a
SohamDesai1 <sohamdesai123@gmail.com>
2023-03-05 17:46:06 +0530

    fix(nextjs@12): downgraded nextjs from 13 to 12 and fixed bugs related to antd and tailwind

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'antd';
+const Home = () => {
+  return (
+    <>
+    <div className='text-red-800'>
+      Home
+    </div>
+    <Button type="primary">Primary Button</Button></>
+  )
+}
+
+export default Home
