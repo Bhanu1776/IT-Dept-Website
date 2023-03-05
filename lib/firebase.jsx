9e6b3c8d2efaa6905ea645410b627073b11e7de7
SohamDesai1 <sohamdesai123@gmail.com>
2023-03-06 00:53:30 +0530

    chore(upload): Handle upload for file storage

--- lib/firebase.jsx
+++ lib/firebase.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getStorage} from "firebase/storage";
 
 const firebaseConfig = {
@@ -16,4 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
--- pages/upload.js
+++ pages/upload.js
@@ -1,41 +1,65 @@
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
-const { Dragger } = Upload;
-const props = {
-    name: 'file',
-    multiple: true,
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    onChange(info) {
-        const { status } = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
+import { storage } from "../lib/firebase";
+import { uploadBytes, ref } from "firebase/storage";
+import { useState } from 'react';
+
+
+const upload = () => {
+    const [file, setFile] = useState([]);
+    const { Dragger } = Upload;
+    const props = {
+        name: 'file',
+        multiple: true,
+        onChange(info) {
+            const { status } = info.file;
+            if (status !== 'uploading') {
+                console.log(info.file, info.fileList);
+                setFile(info.fileList)
+            }
+            if (status === 'done') {
+                message.success(`${info.file.name} file uploaded successfully.`);
+                // setFile(info.file);
+                console.log(file);
+            } else if (status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+        onDrop(e) {
+            console.log('Dropped files', e.dataTransfer.files);
+        },
+    };
+    const handleUpload = async () => {
+        if (file) {
+            const name = file.name;
+            const storageRef = ref(storage, `notes/${name}`);
+            uploadFile = await uploadBytes(storageRef, file);
+
         }
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-    onDrop(e) {
-        console.log('Dropped files', e.dataTransfer.files);
-    },
-};
-const upload = () => (
-    <>
-        <div className='grid h-full place-content-center'>
-            <Dragger {...props} listType='picture' progress={{
-                strokeColor: {
-                    "0%": "#f0f",
-                    "100%": "#ff0"
-                }, strokeWidth: 4,
-                style: { top: 12 }
-            }}>
-                <p className="ant-upload-drag-icon">
-                    <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">Click or Drag and Drop to Upload</p>
-            </Dragger>
-        </div>
-    </>
-);
-export default upload;
+    };
+
+
+    const handleRemoveFile = () => setImageFile(undefined);
+
+
+    return (
+        <>
+            <div className='grid h-full place-content-center'>
+                <Dragger {...props} listType='picture' progress={{
+                    strokeColor: {
+                        "0%": "#f0f",
+                        "100%": "#ff0"
+                    }, strokeWidth: 4,
+                    style: { top: 12 }
+                }}>
+                    <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or Drag and Drop to Upload</p>
+                </Dragger>
+            </div>
+        </>
+    );
+}
+
+export default upload
