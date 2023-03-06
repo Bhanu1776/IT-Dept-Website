d7c56de73c163d6a200ff0b3fd0765db62e8f263
Osama <shaikhosama499@gmail.com>
2023-03-06 10:59:58 +0530

    chore: Updating default files

---
+++ pages/homepage.js
@@ -0,0 +1,19 @@
+import Navbar from "../Components/Navbar";
+import Carousel from "../Components/HeroCarousel";
+import React from "react";
+import Footer from "../Components/Footer";
+import Objectives from "../Components/Objectives";
+import Acedemics from "../Components/Acedemics";
+const homepage = () => {
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+      <Acedemics />
+      <Objectives />
+      <Footer />
+    </>
+  );
+};
+
+export default homepage;
