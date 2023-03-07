447bb1b6c2cf2cf8f2a8ab2c807ce62ae23621df
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-03-07 10:00:36 +0530

    dep(Eslint): Installed Eslint and added new config
    Eslint will imporve the way of writing the code. Fixes common lint issues

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './jsconfig.json',
+  },
+  plugins: [
+
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-console': 0,
+  },
+};
