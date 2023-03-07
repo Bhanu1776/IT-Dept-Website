7ff5f847112c98cb9d6a0bfa4b34b60fa6341077
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-03-07 10:34:14 +0530

    style: Code Formatted
    Using Eslint and Prettier

--- pages/upload.js
+++ pages/upload.js
@@ -1,41 +1,47 @@
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
+
 const { Dragger } = Upload;
 const props = {
-    name: 'file',
-    multiple: true,
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    onChange(info) {
-        const { status } = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-    onDrop(e) {
-        console.log('Dropped files', e.dataTransfer.files);
-    },
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
 };
 const upload = () => (
-    <>
-        <div className='grid w-screen h-screen bg-blue-100 place-content-center' >
-            <Dragger {...props} listType='picture' accept='.pdf' style={{ padding: '30px 300px', maxHeight: '100px', maxWidth: '300px' }} progress={{
-                strokeColor: {
-                    "0%": "#f0f",
-                    "100%": "#ff0"
-                }, strokeWidth: 4,
-                style: { top: 12 }
-            }}>
-                <p className="ant-upload-drag-icon">
-                    <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">Click or Drag and Drop to Upload</p>
-            </Dragger>
-        </div>
-    </>
+  <div className="grid w-screen h-screen bg-blue-100 place-content-center">
+    <Dragger
+      {...props}
+      listType="picture"
+      accept=".pdf"
+      style={{ padding: '30px 300px', maxHeight: '100px', maxWidth: '300px' }}
+      progress={{
+        strokeColor: {
+          '0%': '#f0f',
+          '100%': '#ff0',
+        },
+        strokeWidth: 4,
+        style: { top: 12 },
+      }}
+    >
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">Click or Drag and Drop to Upload</p>
+    </Dragger>
+  </div>
 );
 export default upload;
