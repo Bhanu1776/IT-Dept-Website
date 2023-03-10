c5dce147aa693cba98ec1416628228fb05ba5c05
SohamDesai1 <sohamdesai123@gmail.com>
2023-03-10 23:44:05 +0530

    chore(login): Merged branch Mrunal for login module

--- lib/firebase.jsx
+++ lib/firebase.jsx
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
 // import { getAnalytics } from "firebase/analytics";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGIN_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGIN_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`,
+  measurementId: `${process.env.MEASUREMENT_ID}`,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app, "it-dept-website.appspot.com");
+export const storage = getStorage(app, 'it-dept-website.appspot.com');
+
 // const analytics = getAnalytics(app);
