2eccd01edbb9129d262843db7b094c0a4c0f44f8
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-03-11 18:22:23 +0530

    style(Objectives): Redesigned whole component
    Now its compelety responsive

--- components/Objectives.jsx
+++ components/Objectives.jsx
@@ -1,180 +1,112 @@
-import React from 'react';
-import { Typography, Divider } from 'antd';
-
 const Objectives = () => (
-  <div className="container grid ">
-    <div className="text-center vision">
-      <Divider>
-        <Typography.Title level={2}>Vision</Typography.Title>
-      </Divider>
-      <Typography.Text>
-        To nurture the joy of excellence in the world of Information Technology.
-      </Typography.Text>
+  <>
+    {/* bg-[#f2e8f39e] bg-[#a8f9e84f]  bg-[#fdffba59] */}
+    <div className="grid xxsm:grid-cols-1 md:grid-rows-1  p-8  md:grid-cols-3">
+      <div className="col-span-2 text-center bg-[#f8f9a87a] rounded-xl shadow-md p-6">
+        <h1 className="text-2xl font-bold mb-2">Vision</h1>
+        <p>
+          To nurture the joy of excellence in the world of Information
+          Technology.
+        </p>
+        <h1 className="mt-6 text-2xl font-bold mb-2">Mission</h1>
+        <p>
+          - To develop the critical thinking ability of students by promoting
+          interactive learning.
+        </p>
+        <p>
+          - To bridge the gap between industry and institute and give students
+          the kind of exposure to the industrial requirements in current trends
+          of developing technology.
+        </p>
+        <p>
+          - To promote learning and research methods and make them excel in the
+          field of their study by becoming responsible while dealing with social
+          concerns.
+        </p>
+        <p>
+          - To encourage students to pursue higher studies and provide them
+          awareness on various career opportunities that are available.
+        </p>
+      </div>
+      <div className="p-4 bg-[#a8f9e786] rounded-xl shadow-md text-sm col-span-1 md:text-start xxsm:text-center xxsm:my-4 md:my-0 md:ml-4">
+        <h1 className="mb-3 text-2xl font-bold">Program Outcomes</h1>
+        <p>
+          <span className="font-bold">PO1:</span> Engineering Knowledge
+        </p>
+        <p>
+          <span className="font-bold">PO2:</span> Problem Analysis
+        </p>
+        <p>
+          <span className="font-bold">PO3:</span> Design/Development of
+          Solutions
+        </p>
+        <p>
+          <span className="font-bold">PO4:</span> Conduct Investigations of
+          Complex Problems
+        </p>
+        <p>
+          <span className="font-bold">PO5:</span> Modern Tool Usage
+        </p>
+        <p>
+          <span className="font-bold">PO6:</span> The Engineer and Society
+        </p>
+        <p>
+          <span className="font-bold">PO7:</span> Environment and Sustainability
+        </p>
+        <p>
+          <span className="font-bold">PO8:</span> Ethics
+        </p>
+        <p>
+          <span className="font-bold">PO9:</span> Individual and Team Work
+        </p>
+        <p>
+          <span className="font-bold">PO10:</span> Communication
+        </p>
+        <p>
+          <span className="font-bold">PO11:</span> Project Management and
+          Finance
+        </p>
+        <p>
+          <span className="font-bold">PO12:</span> Life-long Learning
+        </p>
+      </div>
+      <div />
     </div>
-    <div className="mt-5 text-center Mission">
-      <Divider>
-        <Typography.Title level={2}>Mission</Typography.Title>
-      </Divider>
-      <Typography.Paragraph className="leading-none">
-        To develop the critical thinking ability of students by promoting
-        interactive learning.
-      </Typography.Paragraph>
-      <Typography.Paragraph className="leading-none">
-        To bridge the gap between industry and institute and give students the
-        kind of exposure to the industrial requirements in current trends of
-        developing technology.
-      </Typography.Paragraph>
-      <Typography.Paragraph className="leading-none">
-        To promote learning and research methods and make them excel in the
-        field of their study by becoming responsible while dealing with social
-        concerns.
-      </Typography.Paragraph>
-      <Typography.Paragraph className="leading-none">
-        To encourage students to pursue higher studies and provide them
-        awareness on various career opportunities that are available.
-      </Typography.Paragraph>
-    </div>
-    <div className="mt-6 text-center PEO">
-      <Divider>
-        <Typography.Title level={2}>
-          Program Educational Objective (PEO)
-        </Typography.Title>
-      </Divider>
-      <Typography.Paragraph className="leading-none">
-        After 3-5 years of graduation, Information Technology Engineering
-        Graduates shall be :
-      </Typography.Paragraph>
-      <Typography.Paragraph className="leading-none">
-        employed as IT professionals, and shall engage themselves in learning,
-        understanding, and applying newly developed ideas and technologies as
-        their field of study evolves.
-      </Typography.Paragraph>
-      <Typography.Paragraph className="leading-none">
-        competent to use the learnt knowledge successfully in the diversified
-        sectors of industry, academia, research and work effectively in a
-        multidisciplinary environment.
-      </Typography.Paragraph>
-      <Typography.Paragraph className="leading-none">
-        aware of professional ethics and create a sense of social responsibility
-        in building the nation/society.
-      </Typography.Paragraph>
-    </div>
-    <div className="mt-6 text-center PSO">
-      <Divider>
-        <Typography.Title level={2}>
-          Program Specific outcome (PSO)
-        </Typography.Title>
-      </Divider>
-      <Typography.Paragraph className="leading-none">
+
+    <div className="text-center bg-[#f1daf3a6] rounded-xl shadow-lg p-5 mx-8 -mt-8 md:-mt-4 mb-6">
+      <h1 className="text-2xl font-bold mb-3">
+        Program Educational Objectives
+      </h1>
+      <p>
+        <span className="font-bold">PEO1:</span> Information Technology
+        Engineering Graduates shall be employed as IT professionals, and shall
+        engage themselves in learning, understanding, and applying newly
+        developed ideas and technologies as their field of study evolves.
+      </p>
+      <p>
+        <span className="font-bold">PEO2:</span> Information Technology
+        Engineering Graduates shall be competent to use the learnt knowledge
+        successfully in the diversified sectors of industry, academia, research
+        and work effectively in a multidisciplinary environment.
+      </p>
+      <p>
+        <span className="font-bold">PEO3:</span> Information Technology
+        Engineering Graduates shall be aware of professional ethics and create a
+        sense of social responsibility in building the nation/society
+      </p>
+
+      <h1 className="mt-8 text-2xl font-bold mb-2">Program Specific Outcome</h1>
+      <p>
         Demonstrate the ability to analyze and visualize the business domain and
         formulate appropriate information technology solutions.
-      </Typography.Paragraph>
-      <Typography.Paragraph className="leading-none">
+      </p>
+      <p>
         Apply various technologies like Intelligent Systems, Data Mining, IOT,
         Cloud and Analytics, Computer and Network Security etc. for innovative
         solutions to real time problems.
-      </Typography.Paragraph>
+      </p>
     </div>
-    <div className="mt-6 leading-3 text-center PO">
-      <Divider>
-        <Typography.Title level={2}>Program Outcomes (PO)</Typography.Title>
-      </Divider>
-      <Typography.Paragraph className="leading-none">
-        Engineering Graduates will be able to:
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO1-Engineering Knowledge: Apply the knowledge of mathematics, science,
-        engineering fundamentals, and an engineering specialization to the
-        solution of complex engineering problems.
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO2-Problem Analysis: Identify, formulate, review research literature,
-        and analyse complex engineering problems reaching substantiated
-        conclusions using first principles of mathematics, natural sciences and
-        engineering sciences.
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO3-Design/Development of Solutions: Design solutions for complex
-        engineering problems and design system components or processes that meet
-        the specified needs with appropriate consideration for the public health
-        and safety,
-        <Typography.Paragraph>
-          and the cultural, societal, and environmental considerations.
-        </Typography.Paragraph>
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO4-Conduct Investigations of Complex Problems: Use research-based
-        knowledge and research methods including design of experiments, analysis
-        and interpretation of data, and synthesis of the information to provide
-        valid
-        <Typography.Paragraph>
-          conclusions for complex problems.
-        </Typography.Paragraph>
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO5-Modern Tool Usage: Create, select, and apply appropriate techniques,
-        resources, and modern engineering and IT tools including prediction and
-        modelling to complex engineering activities with an understanding of the
-        limitations.
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO-6The Engineer and Society: Apply reasoning informed by the contextual
-        knowledge to assess societal, health, safety, legal and cultural issues
-        and the consequent responsibilities relevant to the professional
-        engineering practice.
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO7-Environment and Sustainability: Understand the impact of the
-        professional engineering solutions in societal and environmental
-        contexts, and demonstrate the knowledge of, and need for sustainable
-        development.
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO8-Ethics: Apply ethical principles and commit to professional ethics
-        and responsibilities and norms of the engineering practice.
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO9-Individual and Team Work: Function effectively as an individual, and
-        as a member or leader in diverse teams, and in multidisciplinary
-        settings.
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO10-Communication: Communicate effectively on complex engineering
-        activities with the engineering community and with society at large,
-        such as, being able to comprehend and write effective reports and design
-        documentation,
-        <Typography.Paragraph>
-          make effective presentations, and give and receive clear instructions.
-        </Typography.Paragraph>
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO11-Project Management and Finance: Demonstrate knowledge and
-        understanding of the engineering and management principles and apply
-        these to one’s own work,as a member and leader in a team,to manage
-        projects and in
-        <Typography.Paragraph>
-          multidisciplinary environments.
-        </Typography.Paragraph>
-      </Typography.Paragraph>
-
-      <Typography.Paragraph className="leading-none">
-        PO12-Life-long Learning: Recognize the need for, and have the
-        preparation and ability to engage in independent and lifelong learning
-        in the broadest context of technological change.
-      </Typography.Paragraph>
-    </div>
-  </div>
+  </>
 );
 
 export default Objectives;
