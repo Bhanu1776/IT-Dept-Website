4c81eced668e09ea994facc385f90520bf6de90b
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-03-11 17:40:36 +0530

    feat(Academics): Added Academics with completely new design
    Initial implementation!

---
+++ components/Academics.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Academics = () => (
+  <main>
+    <div className="mb-6 text-3xl text-center">Academics</div>
+
+    <div className="grid gap-8 px-8 xxsm:grid-flow-row sm:grid-flow-col">
+      <div className="py-4 text-center duration-200 ease-in-out bg-purple-100 shadow-md hover:bg-purple-50 rounded-2xl">
+        SE
+      </div>
+      <div className="py-4 text-center duration-200 ease-in-out bg-teal-100 shadow-md hover:bg-teal-50 rounded-2xl">
+        TE
+      </div>
+      <div className="py-4 text-center duration-200 ease-in-out bg-indigo-100 shadow-md hover:bg-indigo-50 rounded-2xl">
+        BE
+      </div>
+    </div>
+  </main>
+);
+
+export default Academics;
