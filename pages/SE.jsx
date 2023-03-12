012ceb51da7d42765c3bfd062e20e33d3a15cfe2
Osama <shaikhosama499@gmail.com>
2023-03-12 16:04:45 +0530

    chore(SE): SE module added

---
+++ pages/SE.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+
+const SE = () => (
+  <>
+    <Navbar />
+    <Footer />
+  </>
+);
+
+export default SE;
