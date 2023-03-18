530620f4075decdc6d3c081678e93a41dd2c8593
farhan <famannat@gmail.com>
2023-03-18 14:23:47 +0530

    chore(app): Code refactord by eslint. changed padding in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const colors = require('tailwindcss/colors');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class', // or 'media' or 'class'
   important: true,
@@ -16,7 +18,7 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2.4rem",
+      // margin: '10px',
     },
     debugScreens: {
       position: ['bottom', 'left'],
@@ -24,20 +26,20 @@ module.exports = {
       style: {
         backgroundColor: 'black',
         color: 'white',
-        fontSize: '15px'
+        fontSize: '15px',
       },
     },
     screens: {
       xxsm: '320px',
       xsm: '425px',
-      'mobile': '425px',
+      mobile: '425px',
       sm: '640px',
-      'tablet': '640px',
+      tablet: '640px',
       md: '768px',
       lg: '1024px',
-      'laptop': '1024px',
+      laptop: '1024px',
       xl: '1440px',
-      'desktop': '1280px',
+      desktop: '1280px',
       '2xl': '1536px',
     },
     extend: {
@@ -46,7 +48,7 @@ module.exports = {
         mono: [...defaultTheme.fontFamily.mono],
         headings: ['Manrope', ...defaultTheme.fontFamily.sans],
         fancy: ['Sriracha'],
-        fira: ['Fira Sans', 'sans-serif']
+        fira: ['Fira Sans', 'sans-serif'],
       },
       colors: {
         orange: colors.orange,
@@ -60,7 +62,7 @@ module.exports = {
         emerald: colors.emerald,
         fuchsia: colors.fuchsia,
         amber: colors.amber,
-        sky: colors.sky
+        sky: colors.sky,
       },
       typography: (theme) => ({
         DEFAULT: {
@@ -72,38 +74,38 @@ module.exports = {
               textDecorationColor: theme('colors.teal.500'),
               textUnderlineOffset: '3px',
               textDecorationStyle: 'decoration-solid',
-              code: { color: theme('colors.blue.400') }
+              code: { color: theme('colors.blue.400') },
             },
             blockquote: {
               borderLeftColor: theme('colors.teal.500'),
               backgroundColor: theme('colors.gray.50'),
-              color: theme('colors.gray.700')
+              color: theme('colors.gray.700'),
             },
             'h1,h2,h3,h4': {
-              color: theme('colors.gray.900')
+              color: theme('colors.gray.900'),
             },
             // hr: { borderColor: theme('colors.gray.700') },
             strong: { color: theme('colors.gray.700') },
             thead: {
               color: theme('colors.gray.100'),
-              borderBottomColor: theme('colors.gray.600')
+              borderBottomColor: theme('colors.gray.600'),
             },
             tbody: {
               tr: {
-                borderBottomColor: theme('colors.gray.700')
-              }
+                borderBottomColor: theme('colors.gray.700'),
+              },
             },
             hr: {
               color: theme('colors.gray.200'),
-              '&before': { content: '∿∿∿' }
+              '&before': { content: '∿∿∿' },
             },
             code: { color: theme('colors.indigo.500') },
             'blockquote p:first-of-type::before': false,
             'blockquote p:last-of-type::after': false,
             pre: {
-              backgroundColor: theme('colors.gray.100')
-            }
-          }
+              backgroundColor: theme('colors.gray.100'),
+            },
+          },
         },
         dark: {
           css: {
@@ -111,44 +113,49 @@ module.exports = {
             a: {
               color: theme('colors.slate.50'),
               '&:hover': {
-                color: theme('colors.teal.500')
+                color: theme('colors.teal.500'),
               },
               textDecorationColor: theme('colors.teal.400'),
               textUnderlineOffset: '3px',
               textDecorationStyle: 'decoration-solid',
-              code: { color: theme('colors.blue.400') }
+              code: { color: theme('colors.blue.400') },
             },
             blockquote: {
               borderLeftColor: theme('colors.teal.500'),
               backgroundColor: theme('colors.slate.800'),
-              color: theme('colors.gray.400')
+              color: theme('colors.gray.400'),
             },
             'h1,h2,h3,h4': {
-              color: theme('colors.white')
+              color: theme('colors.white'),
             },
             hr: { borderColor: theme('colors.gray.600') },
             strong: { color: theme('colors.gray.100') },
             thead: {
               color: theme('colors.gray.100'),
-              borderBottomColor: theme('colors.gray.600')
+              borderBottomColor: theme('colors.gray.600'),
             },
             tbody: {
               tr: {
-                borderBottomColor: theme('colors.gray.700')
-              }
+                borderBottomColor: theme('colors.gray.700'),
+              },
             },
             code: { color: theme('colors.indigo.200') },
             pre: {
-              backgroundColor: theme('colors.midnight')
-            }
-          }
-        }
-      })
+              backgroundColor: theme('colors.midnight'),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [require("tailwindcss-debug-screens"),
-  require("tailwindcss-brand-colors"),
-  require('@tailwindcss/typography'),
-  require('@tailwindcss/forms')],
-
-}
+  plugins: [
+    // eslint-disable-next-line global-require
+    require('tailwindcss-debug-screens'),
+    // eslint-disable-next-line global-require
+    require('tailwindcss-brand-colors'),
+    // eslint-disable-next-line global-require
+    require('@tailwindcss/typography'),
+    // eslint-disable-next-line global-require
+    require('@tailwindcss/forms'),
+  ],
+};
