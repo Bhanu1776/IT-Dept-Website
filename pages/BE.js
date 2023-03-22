35e576fef6dbd654b29fac42036b2fc08e13f368
Osama <shaikhosama499@gmail.com>
2023-03-22 20:35:38 +0530

    chore(Academics): SE,BE,TE page module added

---
+++ pages/BE.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import BEAccordian from '../components/BEAccordian';
+// import Footer from '../components/Footer';
+
+const BE = () => (
+  <>
+    <Navbar />
+    <BEAccordian />
+    {/* <Footer /> */}
+  </>
+);
+
+export default BE;
---
+++ pages/SE.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Hello from '../components/SEAccordian';
+// import Footer from '../components/Footer';
+
+const SE = () => (
+  <>
+    <Navbar />
+    <Hello />
+    {/* <Footer /> */}
+  </>
+);
+
+export default SE;
