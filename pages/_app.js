23e17930b1899faa17025578c69181dabae41c4e
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-04-03 03:55:35 +0530

    chore(Routing): Admin page routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/globals.css';
 import classNames from 'classnames';
 import { Helmet } from 'react-helmet';
+import { useRouter } from 'next/router';
+import Sidebar from '@/components/Sidebar';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <Helmet>
@@ -13,7 +16,17 @@ function MyApp({ Component, pageProps }) {
         />
       </Helmet>
 
-      <Component {...pageProps} />
+      {router.asPath === '/admin' ||
+      router.asPath === '/uploadNotes' ||
+      router.asPath === '/uploadQB' ||
+      router.asPath === '/uploadTT' ||
+      router.asPath === '/uploadQuizzes' ? (
+        <Sidebar>
+          <Component {...pageProps} />
+        </Sidebar>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </>
   );
 }
