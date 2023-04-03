426deab39af965ecedcbc5f76f003c5fbd7c20d8
Osama <shaikhosama499@gmail.com>
2023-04-03 11:52:57 +0530

    feat(Academics): Pdf Viewer module added

---
+++ components/PdfViewer.jsx
@@ -0,0 +1,27 @@
+// import CPPL from '../files/CPPL Assignment 2.pdf';
+import Navbar from './Navbar';
+// import PDF from '../public/assets/images/files/q6.dsa.pdf';
+
+const PdfViewer = () => (
+  <>
+    <Navbar />
+    <div className=" w-auto h-[100vh] overflow-y-auto flex items-center content-center">
+      {/* <object data={PDF} type="application/pdf" height={100} /> */}
+      <object
+        data="http://africau.edu/images/default/sample.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+      >
+        {/* <p>
+            Alternative text - include a link
+            <a href="http://africau.edu/images/default/sample.pdf">
+              to the PDF!
+            </a>
+          </p> */}
+      </object>
+    </div>
+  </>
+);
+
+export default PdfViewer;
