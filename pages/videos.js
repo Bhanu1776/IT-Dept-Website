22229f446adb675bbbbd13afac7dced3ec3ac43b
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-04-03 13:23:37 +0530

    feat(Videos): Added new page videos
    Styling yet to be done

---
+++ pages/videos.js
@@ -0,0 +1,51 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
+
+// const API_KEY = 'AIzaSyC_Fl28pvpbP4ES5xRiHeUQ2eIrB9BSpsI';
+const API_KEY = process.env.YT_API;
+// console.log('API KEY :', API_KEY);
+// const channelId = 'UCPcOCuFaHs1AuldTgjgtOLW';   //* College ChannelID
+const channelId = 'UCmFt1y9cbHx3amPRctMEyvA'; //* Temporary Testing channelID
+
+const fetchURL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=10`;
+
+const videos = () => {
+  const [allVideos, setAllVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(fetchURL)
+      .then((response) => response.json())
+      .then((resJson) => {
+        const result = resJson.items.map((doc) => ({
+          ...doc,
+          VideoLink: `https://www.youtube.com/embed/${doc.id.videoId}`,
+        }));
+        setAllVideos(result);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+        {allVideos.map((item) => (
+          // eslint-disable-next-line react/jsx-key
+          <div className="flex flex-row">
+            <iframe
+              width="560"
+              height="315"
+              src={item.VideoLink}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default videos;
