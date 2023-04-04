b8187e9a48695594e4b4b1c1a46c5dde36153163
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-04-04 21:27:56 +0530

    style(NewEvents): Redesigned whole component

---
+++ components/landing/NewEventCards.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+
+const NewEventsCards = (props) => (
+  <div className="shadow-xl rounded-lg">
+    <img
+      className="h-[65%] w-full rounded-tl-lg rounded-tr-lg"
+      src={props.cardimg}
+      alt=""
+    />
+    <div className="p-1">
+      <h1 className="font-semibold">{props.eventname}</h1>
+      <h1 className="md:text-[13px] xxsm:text-[10px] 2xl:text-[20px]">
+        It is a long established fact that a reader will be distracted by the
+        readable content
+      </h1>
+    </div>
+  </div>
+);
+
+export default NewEventsCards;
