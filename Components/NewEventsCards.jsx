3b782d98cf8a1d096c226baca7f380acb3cda5a4
itsfarhankhan28 <famannat@gmail.com>
2023-04-06 11:14:20 +0530

    style(NewEventsCards): Changed style of New Events Cards

--- Components/NewEventsCards.jsx
+++ Components/NewEventsCards.jsx
@@ -7,7 +7,7 @@ const NewEventsCards = (props) => {
         <img className='h-[65%] w-full rounded-tl-lg rounded-tr-lg' src={props.cardimg} alt="" />
         <div className='p-1'>
             <h1 className='font-semibold'>{props.eventname}</h1>
-            <h1 className='md:text-[13px] xxsm:text-[10px] 2xl:text-[20px]'>It is a long established fact that a reader will be distracted by the readable content</h1>
+            <h1 className='md:text-[11px] xxsm:text-[10px] 2xl:text-[20px]'>It is a long established fact that a reader will be distracted by the readable content</h1>
         </div>
       </div>
     </>
